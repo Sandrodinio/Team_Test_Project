05c24e001a33b19bc71bdcc660795c63e95a8d9c
domigg23 <domirieder@tsn.at>
2025-03-03 14:34:20 +0100

    updated main

--- src/htl/ah/Main.java
+++ src/htl/ah/Main.java
@@ -1,10 +1,20 @@
 package htl.ah;
 
 public class Main {
+    public static void main(String[] args) {
+        DBManager dbManager = DBManager.getInstance();
 
-	public static void main(String[] args) {
-		System.out.println("Main");
+        Benutzer benutzer1 = new Benutzer("Max", "Mustermann", "1990-01-01");
+        dbManager.speichereBenutzer(benutzer1);
 
-	}
+        Benutzer benutzer2 = new Benutzer("Erika", "Musterfrau", "1985-05-15");
+        dbManager.speichereBenutzer(benutzer2);
 
+        System.out.println("Benutzer geladen:");
+        for (Benutzer b : dbManager.ladeBenutzer()) {
+            System.out.println(b.getVorname() + " " + b.getNachname() + " " + b.getGeburtsdatum());
+        }
+
+        dbManager.loescheBenutzer(benutzer1);
+    }
 }
